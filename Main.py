1c6d3b920b55773fa7239bc63748fe5fc33913ff
POMIDORKUS <kirillvitvinov01@gmail.com>
2024-05-10 18:08:08 +0800

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,60 @@
+base = int(input("СКОЛЬКО ТЕБЕ ЛЕТ: "))
+
+if base > 6:
+    print("ВЕРНОЕ ЭТО БОЛЬШЕ 7")
+    if base < 6:
+        print("ТЫ МАЛКАЯ ГОВНО БРРРРРРРРРРРР")
+        input("GAVNO")
+
+        base = int(input("СКОЛЬКО ТЕБЕ ЛЕТ: "))
+
+if base > 6:
+    print("ВЕРНОЕ ЭТО БОЛЬШЕ 7")
+    if base < 6:
+        print("ТЫ МАЛКАЯ ГОВНО БРРРРРРРРРРРР")
+        input("GAVNO")
+
+        base = int(input("СКОЛЬКО ТЕБЕ ЛЕТ: "))
+
+if base > 6:
+    print("ВЕРНОЕ ЭТО БОЛЬШЕ 7")
+    if base < 6:
+        print("ТЫ МАЛКАЯ ГОВНО БРРРРРРРРРРРР")
+        input("GAVNO")
+        base = int(input("СКОЛЬКО ТЕБЕ ЛЕТ: "))
+
+if base > 6:
+    print("ВЕРНОЕ ЭТО БОЛЬШЕ 7")
+    if base < 6:
+        print("ТЫ МАЛКАЯ ГОВНО БРРРРРРРРРРРР")
+        input("GAVNO")
+        base = int(input("СКОЛЬКО ТЕБЕ ЛЕТ: "))
+
+if base > 6:
+    print("ВЕРНОЕ ЭТО БОЛЬШЕ 7")
+    if base < 6:
+        print("ТЫ МАЛКАЯ ГОВНО БРРРРРРРРРРРР")
+        input("GAVNO")
+        base = int(input("СКОЛЬКО ТЕБЕ ЛЕТ: "))
+
+if base > 6:
+    print("ВЕРНОЕ ЭТО БОЛЬШЕ 7")
+    if base < 6:
+        print("ТЫ МАЛКАЯ ГОВНО БРРРРРРРРРРРР")
+        input("GAVNO")
+        base = int(input("СКОЛЬКО ТЕБЕ ЛЕТ: "))
+
+if base > 6:
+    print("ВЕРНОЕ ЭТО БОЛЬШЕ 7")
+    if base < 6:
+        print("ТЫ МАЛКАЯ ГОВНО БРРРРРРРРРРРР")
+        input("GAVNO")
+        V
+        base = int(input("СКОЛЬКО ТЕБЕ ЛЕТ: "))
+
+if base > 6:
+    print("ВЕРНОЕ ЭТО БОЛЬШЕ 7")
+    if base < 6:
+        print("ТЫ МАЛКАЯ ГОВНО БРРРРРРРРРРРР")
+        input("GAVNO")
+        
